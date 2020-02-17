3b6048d4dec4db102c33788b144000d0f106cd56
Jacob Harrington <jharrington4@gmail.com>
2020-02-17 15:52:11 -0800

    verify route complete and tests passing

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -30,6 +31,25 @@ describe('app routes', () => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           email: 'test@test.com',
+          __v: 0
+        });
+      });
+  });
+
+  it('can verify a users token', async() => {
+    const user = await User.create({ email: 'test@test.com', password: 'password' });
+    const agent = request.agent(app);
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({ email: 'test@test.com', password: 'password' });
+
+    return agent
+      .get('/api/v1/auth/verify')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: user.id,
+          email: user.email,
           __v: 0
         });
       });
